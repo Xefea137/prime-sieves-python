c4c95d6bf982856a01eb0a7040757b18407a31d2
Moe Khan <mobashir16@gmail.com>
2025-04-20 19:17:03 +0530

    Optimized Sieve of Eratosthenes with math.isqrt and optional file output

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -1,23 +1,25 @@
+import math
 import numpy as np
+# np.set_printoptions(threshold=np.inf)
 
-np.set_printoptions(threshold=np.inf)
+def sieve_of_eratosthenes(upper_limit, file_name=None):
+  isPrime = np.ones(upper_limit, dtype=bool)
+  isPrime[0:2] = False
 
-upper_limit = 1000000
+  sqRt = math.isqrt(upper_limit)
+  #sqRt = int(np.sqrt(upper_limit)) + 1
+  for n in range(2, sqRt+1):
+    if isPrime[n]:
+      isPrime[n*n:upper_limit:n] = False
 
-isPrime = np.ones(upper_limit, dtype=bool)
-isPrime[0:2] = False
+  prime = np.flatnonzero(isPrime)
+  
+  if file_name:
+    np.savetxt(file_name, prime, fmt='%d')
+  
+  return prime
 
-sqRt = int(np.sqrt(upper_limit)) + 1
-
-for n in range(2, sqRt):
-  if isPrime[n]:
-    isPrime[n*n:upper_limit:n] = False
-
-prime = np.flatnonzero(isPrime)
-
-if prime[13] == 43:
-  print("OKAY")
-
-print(prime)
-
-np.savetxt('primes.txt', prime, fmt='%d')
+upper_limit = 100000000
+file_name = None #'primes.txt'
+primes = sieve_of_eratosthenes(upper_limit, file_name)
+# print(primes)
