Variable name change and size calculation fix

sieve_of_eratosthenes_triangular_sum.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 
 def sieve_of_eratosthenes(upper_limit, file_name=None):
-  size = upper_limit // 2 + 1
+  size = (upper_limit + 1) // 2
   isPrime = np.ones(size, dtype=bool)
   isPrime[0] = False
 
@@ -12,8 +12,8 @@ def sieve_of_eratosthenes(upper_limit, file_name=None):
   for n in range(1, sqRt//2 + 1):
     triangular_sum += n
     if isPrime[n]:
-      x = (n * 2) + 1
-      isPrime[4*triangular_sum::x] = False
+      step = (n * 2) + 1
+      isPrime[4*triangular_sum::step] = False
 
   prime = np.concatenate(([2], 2*np.flatnonzero(isPrime)+1))
   
@@ -22,7 +22,7 @@ def sieve_of_eratosthenes(upper_limit, file_name=None):
   
   return prime
 
-upper_limit = 100000000
+upper_limit = 1000000000
 file_name = None
 primes = sieve_of_eratosthenes(upper_limit, file_name)
 # print(f"Found {len(primes)} primes. First 10 primes: {primes[:10]}")